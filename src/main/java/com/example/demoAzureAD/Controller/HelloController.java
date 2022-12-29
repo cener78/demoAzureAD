7fc79648f4eef936cafa0d2f9e1815b7f30d9b26
cener78 <94464442+cener78@users.noreply.github.com>
2022-12-29 22:03:50 +0100

    Initial commit

---
+++ src/main/java/com/example/demoAzureAD/Controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demoAzureAD.Controller;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("Admin")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('APPROLE_Admin')")
+    public String Admin() {
+        return "Admin message";
+    }
+
+
+    }
---
+++ src/main/java/com/example/demoAzureAD/DemoAzureAdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoAzureAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAzureAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAzureAdApplication.class, args);
+	}
+
+}
